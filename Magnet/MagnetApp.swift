191ff7b0310a5e58c81f471232883885b4a7cf2d
Muze Lyu <35753187+Lora224@users.noreply.github.com>
2025-05-30 11:18:54 +1000

    Initial Commit

---
+++ Magnet/MagnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnetApp.swift
+//  Magnet
+//
+//  Created by Muze Lyu on 30/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct MagnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
