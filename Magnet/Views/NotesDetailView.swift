a1f093216f3791ec41614f33aa652e4a2c4cd0fe
emmiikey <161409573+emmiikey@users.noreply.github.com>
2025-06-02 10:35:07 +1000

    Basic Layout
    Added header color and stickynote

--- Magnet/Views/NotesDetailView.swift
+++ Magnet/Views/NotesDetailView.swift
@@ -6,3 +6,39 @@
 //
 
 //For checking notes details from the main view
+import SwiftUI
+
+struct NotesDetailView: View {
+    
+    // Hex-based colors as properties
+    private let magnetBrown   = Color(red: 0.294, green: 0.212, blue: 0.129) // #4B3621
+    private let magnetPink    = Color(red: 0.945, green: 0.827, blue: 0.808) // #F1D3CE
+    private let magnetYellow  = Color(red: 1.000, green: 0.961, blue: 0.855) // #FFF5DA
+    private let magnetBlue    = Color(red: 0.820, green: 0.914, blue: 0.965) // #D1E9F6
+
+    
+
+    var body: some View {
+        VStack {
+            
+            Rectangle()
+                .fill(magnetYellow)
+                .frame(height: 100)
+            
+            Image("blueNotePlain")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame( maxWidth: 450)
+                
+            
+            Spacer()
+        }
+        .ignoresSafeArea(.all, edges: .top)
+    }
+}
+
+#Preview(traits: .landscapeLeft) {
+    NotesDetailView()
+}
+
+
