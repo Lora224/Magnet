4a6a54cb27a080742f070d52b97dc70f5e9183b6
YasminYaz <138777259+YasminYaz@users.noreply.github.com>
2025-06-04 11:18:35 +1000

    Improve TextInputView UI

--- Magnet/Views/test.swift
+++ Magnet/Views/test.swift
@@ -5,27 +5,27 @@
 //  Created by Yutong Li on 3/6/2025.
 //
 
-
-import SwiftUI
-import FirebaseFirestore
-
-struct FirestoreNameView: View {
-    @State private var testname: String = "..."
-
-    var body: some View {
-        Text(testname)
-            .font(.title)
-            .onAppear {
-                Firestore.firestore().collection("test1").getDocuments { snapshot, error in
-                    if let doc = snapshot?.documents.first,
-                       let value = doc["testname"] as? String {
-                        testname = value
-                    }
-                }
-            }
-    }
-}
-
-#Preview {
-    FirestoreNameView()
-}
+//
+//import SwiftUI
+////import FirebaseFirestore
+//
+//struct FirestoreNameView: View {
+//    @State private var testname: String = "..."
+//
+//    var body: some View {
+//        Text(testname)
+//            .font(.title)
+//            .onAppear {
+//                Firestore.firestore().collection("test1").getDocuments { snapshot, error in
+//                    if let doc = snapshot?.documents.first,
+//                       let value = doc["testname"] as? String {
+//                        testname = value
+//                    }
+//                }
+//            }
+//    }
+//}
+//
+//#Preview {
+//    FirestoreNameView()
+//}
