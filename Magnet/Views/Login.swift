bc45495971f46c9cc41ef7f88d56a05584153cac
emmiikey <161409573+emmiikey@users.noreply.github.com>
2025-06-06 14:43:08 +1000

    Changes to Login UI

--- Magnet/Views/Login.swift
+++ Magnet/Views/Login.swift
@@ -12,6 +12,8 @@ struct Login: View {
     @State private var password = ""
     @StateObject private var authManager = AuthManager()
 
+    private let magnetBrown = Color(red: 0.294, green: 0.212, blue: 0.129) // #4B3621
+    
     var body: some View {
         ZStack {
             Image("loginBackground")
@@ -25,12 +27,13 @@ struct Login: View {
             VStack(spacing: 30) {
                 VStack(spacing: 8) {
                     Text("Welcome to Magnet!")
-                        .font(.largeTitle)
+                        .font(.system(size: 50, weight: .bold))
+                        .foregroundColor(magnetBrown)
                         .fontWeight(.bold)
                         .multilineTextAlignment(.center)
 
                     Text("A shared fridge to show your loved ones you care")
-                        .font(.title3)
+                        .font(.system(size: 25))
                         .multilineTextAlignment(.center)
                         .foregroundColor(Color(red: 110/255, green: 110/255, blue: 110/255))
                 }
@@ -42,14 +45,14 @@ struct Login: View {
                         .textInputAutocapitalization(.never)
                         .submitLabel(.next)
                         .padding(.vertical, 12)
-                        .padding(.horizontal, 16)
+                        .padding(.horizontal, 30)
                         .background(Color.white.opacity(0.9))
                         .cornerRadius(8)
 
                     SecureField("Password", text: $password)
                         .submitLabel(.done)
                         .padding(.vertical, 12)
-                        .padding(.horizontal, 16)
+                        .padding(.horizontal, 30)
                         .background(Color.white.opacity(0.9))
                         .cornerRadius(8)
                 }
@@ -58,22 +61,24 @@ struct Login: View {
                 HStack {
                     Button("Sign Up") {
                         authManager.register(email: email, password: password)
-                    }
 
+                    }
                     Spacer()
 
                     Button("Log In") {
                         authManager.login(email: email, password: password)
                     }
+
                 }
                 .buttonStyle(.borderedProminent)
                 .frame(maxWidth: 400)
             }
-            .padding(40)
+            .tint(magnetBrown)
+            .font(.system(size: 30, weight: .bold))
+            .padding(60)
             .background(Color.white.opacity(0.85))
             .cornerRadius(20)
             .shadow(radius: 10)
-            .padding()
         }
         .alert(authManager.alertMessage, isPresented: $authManager.showingAlert) {
             Button("OK", role: .cancel) { }
