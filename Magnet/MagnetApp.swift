bad0e912d86057fc934f731d061ea8da7fea37f4
Victor_YT <liyutong0918@outlook.com>
2025-06-06 11:38:08 +1000

    Update MagnetApp.swift

--- Magnet/MagnetApp.swift
+++ Magnet/MagnetApp.swift
@@ -6,15 +6,22 @@
 //
 
 import SwiftUI
-
+import FirebaseCore
 import SwiftData
 
-
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 
 @main
 struct MagnetApp: App {
-
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
   var body: some Scene {
     WindowGroup {
