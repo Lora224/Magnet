437a9101d8760952d864817858d9823c243ddd67
Victor_YT <liyutong0918@outlook.com>
2025-06-12 11:06:04 +1000

    Update ProfileAvatarView.swift

--- Magnet/Views/Components/ProfileAvatarView.swift
+++ Magnet/Views/Components/ProfileAvatarView.swift
@@ -11,18 +11,22 @@ struct ProfileAvatarView: View {
                 .fill(Color.white)
                 .frame(width: 150, height: 150)
                 .overlay(
-                    WebImage(url: avatarURL)
-                        .resizable()
-                        .indicator(.activity)
-                        .scaledToFill()
-                        .background(
+                    ZStack {
+                        if let avatarURL = avatarURL {
+                            WebImage(url: avatarURL)
+                                .resizable()
+                                .scaledToFill()
+                                .transition(.fade(duration: 0.5))
+                        } else {
                             Image("avatarPlaceholder")
                                 .resizable()
                                 .scaledToFill()
-                        )
-                        .clipShape(Circle())
-                        .padding(4)
+                        }
+                    }
+                    .clipShape(Circle())
+                    .padding(4)
                 )
+
                 .shadow(radius: 6)
 
             Button(action: editAction) {
